e2b29a523d85f860a4aa642fda3345aaa333158b
Meyer <jmeyer03@ead.ubc.ca>
2024-06-20 09:16:38 -0700

    pre dataOutput refactoring

--- apps/3pg.cpp
+++ apps/3pg.cpp
@@ -157,11 +157,11 @@ int main(int argc, char* argv[])
 
   if (spatial) {
     // Open output grids. 
-    openOutputGrids( refGrid );
-    std::cout << "Opening regular output grids..." << std::endl;
-    openRegularOutputGrids( refGrid, spMinMY, spMaxMY );
-    std::cout << "Regular output grids opened." << std::endl;
-    std::cout << "Reading points from sample file..." << std::endl;
+    //openOutputGrids( refGrid );
+    //std::cout << "Opening regular output grids..." << std::endl;
+    //openRegularOutputGrids( refGrid, spMinMY, spMaxMY );
+    //std::cout << "Regular output grids opened." << std::endl;
+    //std::cout << "Reading points from sample file..." << std::endl;
     // Open and read sample point file
     readSampleFile( refGrid ); 
     std::cout << "Points read from sample file." << std::endl;
@@ -176,35 +176,27 @@ int main(int argc, char* argv[])
      int lastProgress = -1; 
      std::cout << "Processing..." << cellsTotal << " cells... " << std::endl;
      logger.Log("Processing..." + to_string(cellsTotal) + " cells... ");
-     for (int j = 0; j < cellsTotal ; j++)
-     {
-       //PrintGrids();
-       if (j == 16080)
-         bool test = true;
-       int progress = ((100 * cellsDone) / cellsTotal);
-       if (progress > lastProgress)
-         fprintf(stdout, "Completed %2u%%\r", progress);
-       runTreeModel( spMinMY, spMaxMY, spatial, j);
-       cellsDone++;
-       lastProgress = progress;
-      
-     //   fprintf(logfp, "   row %u\n", j); fflush(logfp);
+
+     for (int i = 0; i < nrows; i++) {
+         for (int j = 0; j < ncols; j++) {
+
+             //calculate/print progress
+             int progress = (100 * cellsDone / cellsTotal);
+             if (progress > lastProgress) {
+                 fprintf(stdout, "Completed %2u%%\r", progress);
+             }
+            
+             int cellIndex = i * ncols + j;
+             runTreeModel(spMinMY, spMaxMY, spatial, cellIndex);
+
+             //increment progress
+             cellsDone++;
+             lastProgress = progress;
+         }
      }
-
-//     fprintf(stdout, "Completed %2u%%\r", 100);
-
-//     // Write output grids. 
-    //writeOutputGrids();
   }
 
-//     // Write output grids. 
-//     writeOutputGrids();
-//   }
-//   else {
-//     runTreeModel(spMinMY, spMaxMY, spatial, 0);
-//   }
-
-  CloseGrids(); //Close all files currently open...
+  //CloseGrids(); //Close all files currently open...
 
   return EXIT_SUCCESS;
 }
