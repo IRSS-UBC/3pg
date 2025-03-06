d7a3ceb7e26d0d3b01fbdd58965e114156f503c5
Meyer <jmeyer03@ead.ubc.ca>
2025-03-05 18:21:46 -0800

    WIP: building static version

--- create_dev.bat
+++ create_dev.bat
@@ -1,2 +1,3 @@
+ECHO y | rmdir build /s 2>null
 mkdir dev_build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_GOOGLE_TESTS=OFF -B dev_build
--- create_release.bat
+++ create_release.bat
@@ -1,2 +1,3 @@
+ECHO y | rmdir release_build /s 2>null
 mkdir release_build
 cmake -DBUILD_SHARED_LIBS=OFF -DBUILD_GOOGLE_TESTS=OFF -B release_build
