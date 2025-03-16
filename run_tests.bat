a4488dd764d91f3626f078e93bed6272d8731f30
Meyer <jmeyer03@ead.ubc.ca>
2025-03-16 14:18:41 -0700

    WIP: add debug bin to path for dynamic exe

--- run_tests.bat
+++ run_tests.bat
@@ -1,5 +1,5 @@
 mkdir test_build
-cmake -D CMAKE_BUILD_TYPE=Debug -DBUILD_GOOGLE_TESTS=ON -B test_build
+cmake -DBUILD_SHARED_LIBS=ON -DBUILD_GOOGLE_TESTS=ON -B test_build
 cmake --build test_build -j 4
 cd test_build/tests
 ctest -C Debug
